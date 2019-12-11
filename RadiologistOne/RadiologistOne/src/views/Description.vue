a7f7753995085359377a8a26cf831f439ef986d4
sebas <sflorezp@unal.edu.co>
2019-12-11 01:38:37 -0500

    Se agregan cambios en demo y description

--- RadiologistOne/RadiologistOne/src/views/Description.vue
+++ RadiologistOne/RadiologistOne/src/views/Description.vue
@@ -1,13 +1,22 @@
 ﻿<template>
   <div class="text-center header">
         <h1>About Our App</h1>
-        <p>
-        </p>
-        <p class="center">
-          Our app, is the best tool to help radiologist reducing routine, time-consuming tasks while it automatically review pending medical images, prioritizing only the highest-risk patient for further evaluation and reducing clinical workload.</p>
-        <p class="center">
-          Initially, our app takes a medical image to see if it automatically detect bulge, protrusion or herniation in the lumbar spinal cord assigning priority to those patients that are more likely to have a stenosis.
-        </p>        
+        <br>
+        <div class="row">
+            <div class="container">
+              <p>
+              Our app, is the best tool to help radiologist reducing routine, time-consuming tasks while it automatically review pending medical images, prioritizing only the highest-risk patient for further evaluation and reducing clinical workload.
+              </p>
+            </div>
+        </div> 
+        <div class="row">
+            <div class="container">
+              <p>
+                Initially, our app takes a medical image to see if it automatically detect bulge, protrusion or herniation in the lumbar spinal cord assigning priority to those patients that are more likely to have a stenosis.
+              </p>
+            </div> 
+          </div>
+        </div>       
   </div>
 </template>
 
@@ -30,4 +39,15 @@ export default {
   padding-right: 10rem;
   padding-top: 1rem;
 }
+
+.container {
+  padding-right: 70px;
+  padding-left: 70px;
+  margin-right: auto;
+  margin-left: auto;
+  font-size: 20px;
+}
+
+
+
 </style>
