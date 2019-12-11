e635d38ba50385afd9b556f6f0565a38e1eba2df
sebas <sflorezp@unal.edu.co>
2019-12-11 04:23:17 -0500

    Se agrega el EDA

--- RadiologistOne/RadiologistOne/src/views/EDA.vue
+++ RadiologistOne/RadiologistOne/src/views/EDA.vue
@@ -1,17 +1,53 @@
 <template>
   <div>
-    <iframe
-      frameborder="0"
-      style="width: 100%; height:450px; border:0"
-      v-bind:src="'https://www.bing.com'"
-    ></iframe>
-    <!--<v-btn color="success">Success</v-btn>-->
-  </div>
+  <v-carousel>
+    <v-carousel-item
+      v-for="(item,i) in items"
+        :key="i"
+        :src="item.src"
+        reverse-transition="fade-transition"
+        transition="fade-transition"
+    ></v-carousel-item>
+  </v-carousel>  
+  </div> 
 </template>
 
 <script>
 export default {
-  name: "EDA"
+  name: "EDA",  
+  data () {
+        return {
+      items: [
+        {
+          src: 'Slides/slide1.PNG',          
+        },
+        {
+          src: 'Slides/slide2.PNG', 
+        },
+        {
+          src: 'Slides/slide3.PNG', 
+        },
+        {
+          src: 'Slides/slide4.PNG', 
+        },
+        {
+          src: 'Slides/slide5.PNG', 
+        },
+        {
+          src: 'Slides/slide6.PNG', 
+        },
+        {
+          src: 'Slides/slide7.PNG', 
+        },
+        {
+          src: 'Slides/slide8.PNG', 
+        },
+        {
+          src: 'Slides/slide9.PNG', 
+        },
+      ],
+    }
+  },
 };
 </script>
 
